cab422c68ce54408b9fb971e14cfe141a1d023e5
dstansby <dstansby@users.noreply.github.com>
2022-01-12 14:38:00 +0000

    Deploying to pages from @ dstansby/soda@f00128fced7f9b6f355c0527b3b8b10f595a9264 🚀

--- soda/plot.py
+++ soda/plot.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta
 import portion
 
 from bokeh.io import output_file, show
-from bokeh.models import MultiChoice, PanTool, BoxZoomTool, FixedTicker
+from bokeh.models import MultiChoice, PanTool, BoxZoomTool, FixedTicker, Div
 from bokeh.plotting import figure
-from bokeh.layouts import gridplot, Spacer
+from bokeh.layouts import gridplot, Spacer, column
 
 from soda.availability import DataProduct
 from soda.trajectory import get_traj
@@ -14,18 +14,16 @@ class DataAvailabilityPlotter:
     def __init__(self, output_filename='soda.html'):
         output_file(output_filename, title='SODA')
         datestr = datetime.now().strftime('%Y-%m-%d')
+        tools = [PanTool(dimensions='width'),
+                 BoxZoomTool(dimensions='width'),
+                 'undo',
+                 'redo',
+                 'reset']
         self.plotter = figure(sizing_mode='stretch_width', plot_height=400,
-                              title=f"Solar Orbiter data availability (last updated {datestr}, daily resolution, all data available at http://soar.esac.esa.int/soar/)",
                               x_axis_type='datetime', y_range=[],
                               x_range=(datetime(2020, 2, 10), datetime.now()),
-                              tools=[PanTool(dimensions='width'),
-                                     BoxZoomTool(dimensions='width'),
-                                     'undo',
-                                     'redo',
-                                     'reset',
-                                     'save'])
+                              tools=tools)
         self.plotter.ygrid.grid_line_color = None
-        self.plotter.xaxis.axis_label = "Date"
         self.plotter.outline_line_color = None
 
         self.all_options = ['SWA-PAS-MOM', 'SWA-PAS-GRND-MOM',
@@ -34,11 +32,12 @@ class DataAvailabilityPlotter:
                             'EUI-FSI304-IMAGE', 'EUI-FSI174-IMAGE',
                             'EUI-HRILYA1216-IMAGE', 'EUI-HRIEUV174-IMAGE',
                             'RPW-BIA-DENSITY',
-                            'EPD-EPT-ASUN-RATES', 'EPD-EPT-SUN-RATES',
+                            'EPD-EPT-SUN-RATES',
                             'EPD-STEP-RATES',
                             ]
 
         self.all_options = sorted(self.all_options)
+        '''
         self.multi_choice = MultiChoice(
             value=self.all_options,
             options=self.all_options,
@@ -46,32 +45,43 @@ class DataAvailabilityPlotter:
             width=200,
             sizing_mode='stretch_height',
             title='Select data products')
+        '''
 
         self.r_plot = figure(sizing_mode='stretch_width', plot_height=150,
-                             x_axis_type='datetime', y_range=[0.3, 1],
+                             x_axis_type='datetime', y_range=[0.25, 1.05],
                              x_range=self.plotter.x_range,
-                             tools=[],
-                             title='Radial distance')
+                             title='Radial distance',
+                             tools=tools)
         self.phi_plot = figure(sizing_mode='stretch_width', plot_height=150,
                                x_axis_type='datetime', y_range=[0, 180],
                                x_range=self.plotter.x_range,
-                               tools=[],
-                               title='Earth-Orbiter angle')
+                               title='Earth-Orbiter angle',
+                               tools=tools)
         self.phi_plot.yaxis[0].ticker = FixedTicker(ticks=[0, 90, 180])
         self.add_trajectory()
 
-        self.layout = gridplot([[self.multi_choice, self.plotter],
-                                [None, self.r_plot],
-                                [None, self.phi_plot],
-                                [None, Spacer()]],
-                               sizing_mode='stretch_width')
+        url = '<a href="http://soar.esac.esa.int/soar/">Solar Oribter Archive</a>'
+        self.title = Div(
+            text=(f"<h1>Solar Orbiter data availability</h1> "
+                  f"Last updated {datestr}, daily resolution, "
+                  f"all data available at the {url}"))
+        self.title.style = {'text-align': 'center'}
 
+        panels = [self.plotter, self.r_plot, self.phi_plot]
+        for p in panels + [self.title]:
+            p.align = 'center'
+        layout = gridplot(panels, ncols=1,
+                          sizing_mode='stretch_width',
+                          toolbar_location='right')
+        self.layout = column([self.title, layout], sizing_mode='stretch_width')
+        # top, right, bottom, left
+        self.layout.margin = (0, 75, 0, 75)
         # Add data
         for desc in self.all_options:
             self.add_interval_data(desc)
 
-        self.multi_choice.js_link("value", self.plotter.y_range, "factors")
-        self.plotter.y_range.factors = self.multi_choice.value
+        # self.multi_choice.js_link("value", self.plotter.y_range, "factors")
+        self.plotter.y_range.factors = self.all_options
 
     def add_interval_data(self, descriptor):
         product = DataProduct(descriptor)
